f04531f2c8dcad004e53897b584ff30bd8acf36d
Daisuke Hashimoto <14502049+daisuke834@users.noreply.github.com>
2020-06-24 15:14:05 +0900

    Added subscriber

--- hello_world/launch/hello_world.launch.py
+++ hello_world/launch/hello_world.launch.py
@@ -5,5 +5,8 @@ def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
             package='hello_world', node_executable='my_publisher',
+            output='screen'),
+        launch_ros.actions.Node(
+            package='hello_world', node_executable='my_subscriber',
             output='screen')
     ])
